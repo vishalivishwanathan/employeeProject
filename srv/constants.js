28265c6812ee71a815b7e19c75a64f2d961e3952
vishalivishwanathan <vishali.jagan1997@gmail.com>
2022-02-10 04:12:33 +0000

    added hana

---
+++ srv/constants.js
@@ -0,0 +1,11 @@
+const a = "10";
+const b= "20";
+const httperrorcode ="417";
+const ID1 =1;
+
+module.exports = {
+a,
+b,
+httperrorcode,
+ID1
+}
--- srv/myservice.js
+++ srv/myservice.js
@@ -1,17 +1,21 @@
 
+const { a, b, httperrorcode, ID1 } = require('./constants');
+const TextBundle = require('@sap/textbundle').TextBundle;
+const bundle = new TextBundle('./_i18n/i18n');
+const cds = require('@sap/cds');
+const { Employees } = cds.entities('com.employees');
 
 module.exports = async (srv) => {
-    const { Employees } = cds.entities('com.employees');
 
-    srv.on("READ", "Employees", async(req, res) => {
-        
+    srv.on("READ", "Employees", async (req, res) => {
+
         const tx = cds.transaction(req);
-
+        console.log(bundle.getText("Text1"));
         const result = await tx.run(
-            
-          // UPDATE(Employees).set({ Name: 'Mohan' }).where({ ID: 1 })
-           //SELECT.from(Employees).where({ID : 1})
-           INSERT.into (Employees).entries ({ID:4,Name:'idk',Age:'30'})
+
+            // UPDATE(Employees).set({ Name: 'Mohan' }).where({ ID: 1 })
+            //SELECT.from(Employees).where({ID : ID1})
+            INSERT.into(Employees).entries({ ID: 4, Name: 'idk', Age: '30' })
         )
         return result;
     }),

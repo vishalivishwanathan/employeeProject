02b4d49d0cb4da99672df49e344219202f53017e
vishalivishwanathan <vishali.jagan1997@gmail.com>
2022-02-11 02:47:50 +0000

    Events changes

--- srv/myservice.js
+++ srv/myservice.js
@@ -13,25 +13,30 @@ module.exports = async (srv) => {
         console.log(bundle.getText("Text1"));
         const result = await tx.run(
 
-            // UPDATE(Employees).set({ Name: 'Mohan' }).where({ ID: 1 })
-            //SELECT.from(Employees).where({ID : ID1})
-            INSERT.into(Employees).entries({ ID: 4, Name: 'idk', Age: '30' })
+            // UPDATE(Employees).set({ Name: 'Man' }).where({ ID: 1 })
+            //SELECT.from(Employees).where({ID : 1})
+            //INSERT.into(Employees).entries({ ID: 4, Name: 'idk', Age: '30' })
         )
         return result;
-    }),
-        // function call event
-        srv.on('functionCall', async (req, res) => {
-            const tx = cds.transaction(req)
-            var vProcedure = 'CALL "test"()'
-            var val = tx.run(vProcedure);
-            return val;
-        });
+    });
+    // function call event
+    srv.on('functionCall', async (req, res) => {
+      
+        const tx = cds.transaction(req);
+        console.log(bundle.getText("Text1"));
+        const result = await tx.run(
+            SELECT.from(Employees)
+        );
+        return JSON.stringify(result);
+    });
 
     // Action call event
-    srv.on('actionCall', async (req, res) => {
-        // const tx = cds.transaction(req)
-        // var vProcedure = 'CALL "test"()'
-        // var val = tx.run(vProcedure);		
-        return;
-    });
+    // srv.on('actionCall', async (req, res) => {
+    //     const tx = cds.transaction(req);
+    //     console.log(bundle.getText("Text1"));
+    //     const result = await tx.run(
+    //         SELECT.from(Employees)
+    //     );
+    //     return JSON.stringify(result);
+    // });
 }
